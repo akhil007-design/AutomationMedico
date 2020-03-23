0d3cb21e2195815b1719a61d54b4fd6ca5f841b8
Akhil Sharma <Akhil.Sharma@LXI.lumedx.com>
2020-03-23 13:35:34 +0530

    Commit: Newcode first time

---
+++ src/main/java/Target/TargetTestcases/App.java
@@ -0,0 +1,13 @@
+package Target.TargetTestcases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
